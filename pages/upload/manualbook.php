dad221b26ba35114ea0214fbe91f00da5df13ce8
Rizki Ramadhan <rzk.ramadhan@gmail.com>
2022-03-06 09:02:33 +0700

    Update Button Download -> download file, dan disable option select

--- pages/upload/manualbook.php
+++ pages/upload/manualbook.php
@@ -70,7 +70,7 @@
                                 <td><?php echo $row['DATE']?></td>
                                 <td><?php echo $row['USER']?></td>
                                 <td><?php echo $row['DOC_TYPE']?></td>
-                                <td><a target="_blank" href="../../assets/uploaded/<?php echo $row['FILE_NAME']?>"><i class="fa fa-download"></i></a></td>
+                                <td><a target="_blank" href="../../assets/uploaded/<?php echo $row['FILE_NAME']?>" download="<?php echo $row['FILE_NAME']?>"><i class="fa fa-download"></i></a></td>
                             </tr>
                         <?php }
                     ?>
@@ -82,13 +82,9 @@
         <div class="boxform">
             <h1>Upload Document</h1>
             <form class="form" action="../../phpcode/upload.php" method="post" enctype="multipart/form-data">
-                <label for="namadokumen">Select Document Type </label>
-                <select id="doctype" name="doctype">
-                    <option value="Nota Dinas">Nota Dinas</option>
-                    <option value="Manual Book" selected>Manual Book</option>
-                    <option value="Berita Acara Kejadian">Berita Acara Kejadian</option>
-                    <option value="Single Line Diagram">Single Line Diagram</option>
-                    <option value="Other Document">Other Document</option>
+            <label for="namadokumen">Info: This is type of Document you'll upload :</label>
+                <select id="namadokumen" name="doctype" >
+                    <option value="Manual Book" selected disabled>Manual Book</option>
                 </select>
                 <input id="uploadBtn" name="uploadBtn" type="file" accept="application/pdf" onChange="getName()">
                 <input class="upSubmit" type="submit" value="Submit" name="upload">
